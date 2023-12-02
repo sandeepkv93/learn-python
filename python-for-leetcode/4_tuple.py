ef6111cb41cfe3f4d979b0332c3e791974aa3105
Sandeep Vishnu <sandeepkv93@gmail.com>
2023-12-02 21:30:33 +0530

    Tuple

--- python-for-leetcode/4_tuple.py
+++ python-for-leetcode/4_tuple.py
@@ -1,14 +1,23 @@
-tuple = (1, 2, 3, 1)
+tuple1 = (1, 2, 3, 1)
 
 # 1. Get the length of a tuple
-print(len(tuple))  # Prints 4
+print(len(tuple1))  # Prints 4
 
 # 2. Get the index of an element
-print(tuple.index(1))  # Prints 0
+print(tuple1.index(1))  # Prints 0
 
 # 3. Count the number of occurrences of an element
-print(tuple.count(1))  # Prints 2
+print(tuple1.count(1))  # Prints 2
 
 # 4. Iterate over a tuple
-for element in tuple:
+for element in tuple1:
     print(element)
+
+st = "car"
+count = [0] * 26
+for c in st:
+    count[ord(c) - ord("a")] += 1
+
+# Convert count to tuple
+tuple2 = tuple(count)
+print(tuple2)
