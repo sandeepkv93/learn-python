2daf698e16a615f3721be99d761312dcf7cc5d18
Sandeep Vishnu <sandeepkv93@gmail.com>
2023-12-02 14:29:06 +0530

    Tuple & Heap

---
+++ python-for-leetcode/4_tuple.py
@@ -0,0 +1,14 @@
+tuple = (1, 2, 3, 1)
+
+# 1. Get the length of a tuple
+print(len(tuple))  # Prints 4
+
+# 2. Get the index of an element
+print(tuple.index(1))  # Prints 0
+
+# 3. Count the number of occurrences of an element
+print(tuple.count(1))  # Prints 2
+
+# 4. Iterate over a tuple
+for element in tuple:
+    print(element)
---
+++ python-for-leetcode/5_heap.py
@@ -0,0 +1,17 @@
+import heapq
+
+nums = [5, 7, 9, 1, 3]
+
+heapq.heapify(nums)  # Min heap
+
+print(heapq.heappop(nums))  # Prints 1
+
+heapq.heappush(nums, 4)
+
+heapq.heapreplace(nums, 2)  # Pop and push
+print(nums)  # Prints [2, 4, 9, 7, 5]
+
+# Max heap
+max_heap = [-x for x in nums]
+heapq.heapify(max_heap)
+print(-heapq.heappop(max_heap))  # Prints 9
