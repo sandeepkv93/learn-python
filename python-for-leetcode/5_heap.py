46d38d4d31aff17b88c524d17dd7ca42c1ff3153
Sandeep Vishnu <sandeepkv93@gmail.com>
2023-12-02 14:31:47 +0530

    Heap Continued

--- python-for-leetcode/5_heap.py
+++ python-for-leetcode/5_heap.py
@@ -15,3 +15,8 @@ print(nums)  # Prints [2, 4, 9, 7, 5]
 max_heap = [-x for x in nums]
 heapq.heapify(max_heap)
 print(-heapq.heappop(max_heap))  # Prints 9
+
+# Create a min heap of lists with custom comparator
+nums = [[1, 2], [3, 4], [1, -1]]
+heapq.heapify(nums)
+print(heapq.heappop(nums))  # Prints [1, -1]
